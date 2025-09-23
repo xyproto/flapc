582926d93fe2ba3af5246295d76f95adc7620c96
Alexander F. Rødseth <rodseth@gmail.com>
2025-09-23 14:19:38 +0200

    Use the standard ELF header size

--- elf.go
+++ elf.go
@@ -1,39 +1,35 @@
 package main
 
 func (o *Out) WriteELF() error {
-	size := 0
-	size += o.Write(0x7f)
-	size += o.Write(0x45)                       // E
-	size += o.Write(0x4c)                       // L
-	size += o.Write(0x46)                       // F
-	size += o.Write(2)                          // 64-bit
-	size += o.Write(1)                          // little endian
-	size += o.Write(1)                          // ELF version
-	size += o.Write(3)                          // Linux
-	size += o.Write(3)                          // ABI version, dynamic linker version
-	size += o.WriteN(0, 7)                      // zero padding, length of 7
-	size += o.Write2(2)                         // object file type: executable
-	size += o.Write2(0x3e)                      // machine (AMD x86-64), ARM64 is 0xB7, RISC-V is 0xF3
-	size += o.Write4(1)                         // original ELF version (?)
-	const startAddr = 0x80                      // ?
-	size += o.Write8(startAddr)                 // address of entry point
-	size += o.Write8(0x40)                      // program header table
-	const sectionAddr = 0xff                    // ?
-	size += o.Write8(sectionAddr)               // start of section header table
-	size += o.Write4(0)                         // "interpretation of this field depends on the target architecture"
-	size += o.Write2(64)                        // size of this ELF header
-	size += o.Write2(0x38)                      // size of a program header table entry
-	const programHeaderTableEntries = 1         // ?
-	size += o.Write2(programHeaderTableEntries) // number of entries in the program header table
-	size += o.Write2(0x40)                      // size of a section header table entry
-	const sectionHeaderTableEntries = 1         // ?
-	size += o.Write2(sectionHeaderTableEntries)
+	o.Write(0x7f)
+	o.Write(0x45)                       // E
+	o.Write(0x4c)                       // L
+	o.Write(0x46)                       // F
+	o.Write(2)                          // 64-bit
+	o.Write(1)                          // little endian
+	o.Write(1)                          // ELF version
+	o.Write(3)                          // Linux
+	o.Write(3)                          // ABI version, dynamic linker version
+	o.WriteN(0, 7)                      // zero padding, length of 7
+	o.Write2(2)                         // object file type: executable
+	o.Write2(0x3e)                      // machine (AMD x86-64), ARM64 is 0xB7, RISC-V is 0xF3
+	o.Write4(1)                         // original ELF version (?)
+	const startAddr = 0x80              // ?
+	o.Write8(startAddr)                 // address of entry point
+	o.Write8(0x40)                      // program header table
+	const sectionAddr = 0xff            // ?
+	o.Write8(sectionAddr)               // start of section header table
+	o.Write4(0)                         // "interpretation of this field depends on the target architecture"
+	o.Write2(64)                        // size of this ELF header
+	o.Write2(0x38)                      // size of a program header table entry
+	const programHeaderTableEntries = 1 // ?
+	o.Write2(programHeaderTableEntries) // number of entries in the program header table
+	o.Write2(0x40)                      // size of a section header table entry
+	const sectionHeaderTableEntries = 1 // ?
+	o.Write2(sectionHeaderTableEntries)
 	const sectionHeaderTableEntryIndex = 0 // ?
-	size += o.Write2(sectionHeaderTableEntryIndex)
-	o.Write(byte(size))
-
-	//psize := 0
-	//psize += o.Write4()
+	o.Write2(sectionHeaderTableEntryIndex)
+	o.Write(64)
 
 	return nil
 }
